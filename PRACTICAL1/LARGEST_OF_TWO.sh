fa8aefb058b444788179dfa7e4bc45072b36af46
Sanidhya Pawar <37259966+pawarss1@users.noreply.github.com>
2019-03-11 22:12:08 +0530

    Update LARGEST_OF_TWO.sh

--- PRACTICAL1/LARGEST_OF_TWO.sh
+++ PRACTICAL1/LARGEST_OF_TWO.sh
@@ -1,11 +1,16 @@
+#shell script to find the largest of two numbers
+echo "Enter Num1"
 read a;
+
+echo "Enter Num2"
 read b;
+
 if [ $a = $b ]
 then
-echo both are equal;
+  echo "both are equal";
 elif [  $a -gt $b ]
 then
-echo $a is greater;
+  echo "$a is greater";
 else
-echo $b is greater;
+  echo "$b is greater";
 fi
