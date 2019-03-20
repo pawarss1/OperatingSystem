4d064715dad29ad93e1138fe3553ed8baed36f9a
Sanidhya Pawar <37259966+pawarss1@users.noreply.github.com>
2019-03-20 10:51:27 +0530

    Update bankers.py

--- Practical6/bankers.py
+++ Practical6/bankers.py
@@ -1,3 +1,5 @@
+#BANKERS ALGORITHM..
+
 allocated = [[0, 1, 0],
         [2, 0, 0],
         [3, 0, 2],
@@ -14,45 +16,66 @@ maxNeed = [[7, 5, 3],
 
 process = 5
 resources = 3
-counter = process
-running = [1]*len(process) 
+#counter = process
+running = [1]*process 
 available = [3, 3, 2]
-need = [0]*len(maxNeed)
+maxAvailable = [10, 5, 7]
+
+need = [[0,0,0],
+        [0,0,0],
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+]
 safe = 0
 
 for i in range(process):
     for j in range(resources):
         need[i][j] = maxNeed[i][j] - allocated[i][j]
 
+def breakWhileUnsafeState():
+    safe = not safe
 
 
 
-def bankers():
+def bankers(counter):
+    #print(need)
+    deadlockCtr = 0
     flag = 0
     while(counter != 0):
         safe = 0
         for i in range(process):
             if(running[i]):
                 flag = 1
+                deadlockCtr = counter
                 for j in range(resources):
-                    if(maxNeed[i][j] - allocated[i][j] > available[j]):
+                    if(need[i][j] > available[j]):
                         flag = 0
+                        deadlockCtr -=1
+                        if(deadlockCtr == 0):
+                            breakWhileUnsafeState()
+                            return -1
                         break
                 
                 if(flag):
-                    print("Process", i+1, " is executing")
+                    print("Process", i, " is executing")
                     running[i] = 0
                     safe = 1
                     counter = counter - 1
-
+                
                     for j in range(resources):
                         available[j] += allocated[i][j]
+                    print("AVAILABLE RESOURCES AFTER PROCESS ",i, "ARE", available)
                     break
 
     if(safe):
         print("The processes are in safe state")
+        print(available)
+    
     elif(not safe):
         print("The processes are not in safe state")
 
-
-bankers()
+counter = 5
+result = bankers(counter)
+if(result == -1):
+    print("The processes are not in safe state")
